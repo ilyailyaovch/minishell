d80967cfe5c637db5cb08c780a5bae4ecebc1675
ilyailyaovch <64081099+ilyailyaovch@users.noreply.github.com>
2022-05-02 17:30:17 +0300

    Add files via upload

--- src/minishell.c
+++ src/minishell.c
@@ -6,7 +6,7 @@
 /*   By: pleoma <pleoma@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/03 10:45:36 by pleoma            #+#    #+#             */
-/*   Updated: 2022/05/02 16:15:16 by pleoma           ###   ########.fr       */
+/*   Updated: 2022/05/02 17:29:57 by pleoma           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,14 +16,10 @@ void	ft_readline(void)
 {
 	g_shell.line = readline(GREEN"myshell> "WTH);
 	if (!g_shell.line)
-	{
-		printf(RED"myshell>  exit\n"WTH);
-		exit(0);
-	}
+		ft_shell_error("exit myshell", 1, 1);
 	if (!ft_strncmp(g_shell.line , "exit", 4))
 		ft_shell_error("exit", 0, EXIT_FLAG);
 	add_history(g_shell.line);
-	//free(str);
 }
 
 void	minishell(void)
@@ -33,7 +29,7 @@ void	minishell(void)
 	{
 		//ft_init();
 		ft_readline();
-		//if (*t_shell.line)
+		//if (*t_shell.line && ft_strlen(g_line.line))
 		// {
 		//	if (check_quots_pipe())
 			// 	continue ;
